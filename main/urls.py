70e4dab160921ff73964a6b3f13f4f3958a7c9ca
flavio <flaviodesouzamarques@gmail.com>
2021-02-14 18:08:03 +0000

    Django With Forms

--- main/urls.py
+++ main/urls.py
@@ -3,6 +3,6 @@ from main import views
 
 urlpatterns = [
     path("<int:id>", views.index, name="index"),
-    path("", views.home, name = "home"),
-
+    path("", views.home, name="home"),
+    path("create", views.create, name="create"),
 ]
